310ad104e1bbbeee28b60e5f21fbbd5fcf19d937
Diego Justino <diegojsilva01@outlook.com>
2024-04-27 15:02:47 -0300

    first commit

---
+++ controles.c
@@ -0,0 +1,73 @@
+enum key {
+knone, kup, kdown, kleft, kright
+};
+enum button {
+bnone=0, ba=1, bb=2
+};
+// ponteiros para as funções da interface
+typedef void (*PTRDESCRIPTION)();
+typedef void (*PTRRESET)();
+typedef void (*PTRUPDATE)();
+typedef void (*PTRCONTROLLER)(int key, int button);
+// “classe” interface
+struct my_game
+{
+// Referencia da dll
+HMODULE impl;
+// Ponteiros para as funções da Interface
+PTRDESCRIPTION description;
+PTRRESET reset;
+PTRUPDATE update;
+PTRCONTROLLER controller;
+};
+
+void load_game(const char *game_library, struct my_game *game)
+{
+game->impl = LoadLibraryA(game_library);
+game->description = (PTRDESCRIPTION) GetProcAddress(game->impl,
+"description");
+game->reset = (PTRRESET) GetProcAddress(game->impl, "reset");
+game->update = (PTRUPDATE) GetProcAddress(game->impl, "update");
+game->controller = (PTRCONTROLLER) GetProcAddress(game->impl,
+"controller");
+}
+void flush()
+{
+int c;
+while ((c = getchar()) != '\n' && c != EOF) { }
+}
+int main(int argc, char *argv[])
+{
+struct my_game game;
+load_game(argv[1], &game);
+game.description();
+game.reset();
+do {
+game.update();
+char x = getchar(); flush();
+switch(x) {
+case 'w':
+case 'W':
+game.controller(kup, bnone); break;
+case 's':
+case 'S':
+game.controller(kdown, bnone); break;
+case 'a':
+case 'A':
+game.controller(kleft, bnone); break;
+case 'd':
+case 'D':
+game.controller(kright, bnone); break;
+case 'n':
+case 'N':
+game.controller(knone, ba); break;
+case 'm':
+case 'M':
+game.controller(knone, bb); break;
+case 'r':
+case 'R':
+game.reset(); break;
+}
+} while (1);
+return 0;
+}
